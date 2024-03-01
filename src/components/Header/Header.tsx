22dc9d5dc2540949d25e830d74c0281a701bb87a
Ilya Skorokhodov <skorox2000@gmail.com>
2024-03-01 12:05:38 +0300

    Finished Header component
    Написал основные стили для компонента шапки, добавил svg звезды

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
+import star from '../../assets/star.svg'
+import './Header.css'
 
 export default function Header() {
   return (
     <div className="header">
-      <Link to="/favorites">Избранное</Link>
+      <Link to="/favorites" className="favoriteLink">
+        Избранное
+        <img src={star}/>
+      </Link>
     </div>
   );
 }
